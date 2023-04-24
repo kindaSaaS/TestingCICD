27ecfd8384591879d4c0b4f6ea306d5c4bb7c616
eliarshin <80040465+eliarshin@users.noreply.github.com>
2023-04-24 17:33:59 +0300

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,7 +1,16 @@
 Java.perform(function() {
     var Jenkins = Java.use('jenkins.model.Jenkins');
-    Jenkins.getACL.implementation = function() {
-        console.log('getACL called!');
-        return this.getACL();
+    var Queue = Java.use('jenkins.model.Jenkins$Queue');
+    var Run = Java.use('hudson.model.Run');
+    var ParametersAction = Java.use('hudson.model.ParametersAction');
+
+    Jenkins.buildWithParameters.implementation = function(params) {
+        console.log('Build parameters:', JSON.stringify(params));
+        var queueItem = Queue.getInstance().getItem(this, params.job);
+        var run = Run.fromQueueItem(queueItem);
+        if (run != null) {
+            console.log('Build URL:', run.getUrl());
+        }
+        return this.build(params.job);
     };
 });
