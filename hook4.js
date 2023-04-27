4855fddb639d2e06ac33ba3a424a8be7bc227aee
eliarshin <80040465+eliarshin@users.noreply.github.com>
2023-04-27 14:15:12 +0300

    Update hook4.js

--- hook4.js
+++ hook4.js
@@ -1,8 +1,10 @@
-var target = Process.getFromName("jenkins");
+// Attach to the target process with PID 4121
+var target = Process.get(4121);
 
 // Define the function to hook
 var httpRequest = Module.findExportByName("libjenkins-core.so", "_ZN15hudson/Functions10httpGetURLERKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEj");
 
+// Intercept the HTTP request function
 Interceptor.attach(httpRequest, {
     onEnter: function(args) {
         console.log("HTTP request made to URL: " + args[0].readUtf8String());
