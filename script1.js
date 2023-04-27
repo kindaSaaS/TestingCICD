8db0c323291284c429b628bf74b7e9d1905571a0
eliarshin <80040465+eliarshin@users.noreply.github.com>
2023-04-27 14:18:40 +0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,17 @@
+// Define the constructor signature
+var runConstructor = new NativeFunction(ptr("0x12345678"), 'void', ['pointer', 'pointer', 'pointer']);
+
+// Intercept the constructor call
+Interceptor.attach(runConstructor, {
+    onEnter: function(args) {
+        // args[0] is the this pointer
+        // args[1] is the hudson.model.Job object
+        // args[2] is the hudson.model.Environment object
+
+        // Log the arguments to the constructor
+        console.log("Run constructor called with arguments:");
+        console.log("this: " + args[0]);
+        console.log("job: " + args[1]);
+        console.log("env: " + args[2]);
+    }
+});
